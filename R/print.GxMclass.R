1470175f2a19403d9c4ec29f316427f89aa4058d
Hao Zheng <haozheng@stat.wisc.edu>
2013-10-08 00:00:00 +0000

    version 1.0

---
+++ R/print.GxMclass.R
@@ -0,0 +1,14 @@
+print.GxMclass = function(x, ...) {
+
+    GxMmle = x;
+    cat("Log-likelihood:\t", round(slot(GxMmle,"loglikelihood"),digits=2));
+    cat("\n\n");
+
+    cat("BIC:\t", round(slot(GxMmle,"BIC"),digits=2));
+    cat("\n\n");
+
+    cat("Coefficients:\n")
+    print.default(round(slot(GxMmle,"par"), digits=4), print.gap=2, quote = FALSE)
+    cat("\n")
+    invisible(GxMmle);
+}
